f88a29667dd70a15273b85dd779f14fbf29cd000
JonBrown <Jon.Brown@edfenergy.com>
2018-08-11 22:33:30 +0100

    Added data exploratory charts
    Using Seaborn charting

--- tool/main.py
+++ tool/main.py
@@ -1,22 +1,48 @@
 
-from pandas import read_csv
+import pandas as pd
 import toml
 from os import path, makedirs
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def run_tool(configFN, testing=False):
     
     dictConfig = toml.load(configFN)
     dictGlobal = dictConfig['global']
 
-    dfGender = read_csv(path.join(dictGlobal['inputFP'],dictGlobal['genderFN']))
+    #get passenger survived - PassengerID, Survived
+    dfGender = pd.read_csv(path.join(dictGlobal['inputFP'],dictGlobal['genderFN']))
     dfGender.info()
     print(dfGender.head())
-    dfTest = read_csv(path.join(dictGlobal['inputFP'],dictGlobal['testFN']))
+    #get passenger detailed info - 'PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'
+    dfTest = pd.read_csv(path.join(dictGlobal['inputFP'],dictGlobal['testFN']))
     dfTest.info()
     print(dfTest.head())
-    dfTrain = read_csv(path.join(dictGlobal['inputFP'],dictGlobal['trainFN']))
+    #join whether survived to passenger detailed info
+    dfCombined = pd.merge(dfGender, dfTest, how='inner', on='PassengerId')
+    print(dfCombined.head())
+    #418 rows.  Age 332 rows, Fare 417 rows, Cabin 91 rows
+
+    dfTrain = pd.read_csv(path.join(dictGlobal['inputFP'],dictGlobal['trainFN']))
     dfTrain.info()
     print(dfTrain.head())
+    #891 rows.  Age only 714 rows.  Cabin only 204 rows.
+
+    dfClean = dfCombined[['Survived', 'Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked']]
+
+    sns.pairplot(dfClean, hue='Survived', kind='reg')
+    plt.show()
+
+    sns.lmplot(x='Pclass', y='Survived', data=dfCombined)
+    sns.lmplot(x='Pclass', y='Survived', data=dfCombined, col='Sex')
+    sns.lmplot(x='Pclass', y='Survived', data=dfCombined, row='Sex')
+    plt.show()
+
+    sns.jointplot(x='Pclass',y='Survived',data=dfCombined)
+    plt.show()
+
+
+    
     outputFP = dictConfig['testing']['outputFP'] if testing else dictGlobal['outputFP']
 
 if __name__ == '__main__':
